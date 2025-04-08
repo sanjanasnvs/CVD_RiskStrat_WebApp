b589421b45b2cc874dc47c1b333d594f2fc9389e
Sanjana Srinivasan <sanjanasrini30@gmail.com>
2025-04-08 11:18:09 +0100

    Expanded database for adding questionnaires and response options

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .legacy_models import Patients, Clinicians, Admins, ClinicianPatient, Users, Questionnaire, Responses, PatientOutcomes, RiskStratification, MlModels
+from .legacy_models import Patients, Clinicians, Admins, ClinicianPatient, Users, Questionnaire, Responses, QuestionResponseOptions, PatientOutcomes, RiskStratification, MlModels
 
 
 admin.site.register(Patients)
@@ -10,6 +10,7 @@ admin.site.register(ClinicianPatient)
 admin.site.register(Users)
 admin.site.register(Questionnaire)
 admin.site.register(Responses)
+admin.site.register(QuestionResponseOptions)
 admin.site.register(PatientOutcomes)
 admin.site.register(RiskStratification)
 admin.site.register(MlModels)
