1b907eee617102e4fe1b02fbd7ac55999dd3bf4e
Sanjana Srinivasan <sanjanasrini30@gmail.com>
2025-05-21 16:13:20 +0100

    added question_type to Questionnaire table

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = Users
-        fields = ('username', 'email', 'role', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2')
 
     def save(self, commit=True):
         user = super().save(commit=False)
